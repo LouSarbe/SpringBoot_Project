f7b53c055ae8618be208cd911b7f271b0ea4b7a2
LouSarbe <louis.bessard@edu.devinci.fr>
2024-09-23 16:42:15 +0200

    added a greeting endpoint

--- src/main/java/com/springboot_project/SpringbootProjectApplication.java
+++ src/main/java/com/springboot_project/SpringbootProjectApplication.java
@@ -1,4 +1,4 @@
-package com.springboot_project.springboot_project;
+package com.springboot_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
